ec37842cf37d9bdb4f9c3e2c578262b8f5b76ee2
ab-sid <absid24@gmail.com>
2025-07-26 14:42:55 +0600

    book done

--- src/app/controller/book.controller.ts
+++ src/app/controller/book.controller.ts
@@ -3,17 +3,56 @@ import { Book } from "../model/book.model";
 
 export const bookRouter = express.Router();
 
-bookRouter.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+bookRouter.get("/", async (req: Request, res: Response) => {
+  const { filter, sortBy = "createdAt", sort = "desc", limit = 10 } = req.query;
+  const query: any = {};
+  if (filter) {
+    query.genre = filter;
+  }
+  const books = await Book.find(query)
+      .sort({ [sortBy as string]: sort === 'asc' ? 1 : -1 })
+      .limit(Number(limit));
+  res.status(200).json({
+    success: true,
+    message: "Books retrieved successfully",
+    data: books,
+  });
+});
+
+bookRouter.get("/:bookId", async (req: Request, res: Response) => {
+  const book = await Book.findById(req.params.bookId);
+  res.status(200).json({
+    success: true,
+    message: "Books retrieved successfully",
+    data: book,
+  });
 });
 
 bookRouter.post("/", async (req: Request, res: Response) => {
-  const bookBody = req.body;
-  const book = await Book.create(bookBody);
-
+  const book = await Book.create(req.body);
   res.status(201).json({
     success: true,
     message: "Book created successfully",
     data: book,
+  });
+});
+
+bookRouter.put("/:bookId", async (req: Request, res: Response) => {
+  const book = await Book.findByIdAndUpdate(req.params.bookId, req.body, {
+    new: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Books updated successfully",
+    data: book,
+  });
+});
+
+bookRouter.delete("/:bookId", async (req: Request, res: Response) => {
+  const book = await Book.findByIdAndDelete(req.params.bookId);
+  res.status(200).json({
+    success: true,
+    message: "Books deleted successfully",
+    data: null,
   });
 });
