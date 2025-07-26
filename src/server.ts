2bb6d55bd2ef2ca475f951ad13fcf327993d49cb
ab-sid <absid24@gmail.com>
2025-07-26 10:14:44 +0600

    server setup

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+let server;
+const port = 5000
+
+async function main() {
+  try {
+    await mongoose.connect('mongodb+srv://libraryDB:libraryDB@cluster0.fk0de.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    console.log('db connected');
+    server = app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+  } catch (error) {
+    console.log(error);
+  }
+
+  
+}
+
+main();
